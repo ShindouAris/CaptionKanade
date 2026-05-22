f0a992e9fa76ccdd637c95cb3fde94738c88539a
Chisadin <tranthanhnam.ccc@gmail.com>
2026-05-22 22:39:54 +0700

    fix: update background image upload visibility to allow access for admin only

--- src/components/CaptionBuilder.tsx
+++ src/components/CaptionBuilder.tsx
@@ -286,8 +286,8 @@ const CaptionBuilder: React.FC = () => {
             )}
           </div>
 
-          {/* Background Image Upload [disabled for now] */}
-          {captionUser.isMember && (
+          {/* Background Image Upload [disabled for everyone except admin] */}
+          {captionUser.isMember || user?.id === "0fd2189f-1873-42bb-b2c8-6443772d12e3" && (
             <BackgroundImageUpload
               bgFile={bgFile}
               bgPreview={bgPreview}
